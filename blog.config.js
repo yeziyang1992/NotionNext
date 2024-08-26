a54fe873a8912b30f3cc80fdf197106d01142c84
yeziyang <yeziyang1992@163.com>
2024-08-26 20:43:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'YangZiye', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '不积跬步,无以至千里.不积小流,无以成江海', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https:// yangziye.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://yangziye.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
